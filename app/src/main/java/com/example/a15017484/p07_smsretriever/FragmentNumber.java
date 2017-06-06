c526bd400b780cdfe81c9773b1992a92777a395e
actuallycoding <15017484@myrp.edu.sg>
2017-06-06 12:58:31 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a15017484/p07_smsretriever/FragmentNumber.java
@@ -0,0 +1,105 @@
+package com.example.a15017484.p07_smsretriever;
+
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.PermissionChecker;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentNumber extends Fragment {
+    TextView tvSms;
+    Button btnRetrieve;
+    EditText etNumber;
+
+    public FragmentNumber() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_number, container, false);
+        tvSms = (TextView) view.findViewById(R.id.tvSMS1);
+        btnRetrieve = (Button) view.findViewById(R.id.btnRetrieve1);
+        etNumber = (EditText) view.findViewById(R.id.etNumber);
+
+        btnRetrieve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int permissionCheck = PermissionChecker.checkSelfPermission
+                        (getActivity(), Manifest.permission.READ_SMS);
+
+                if (permissionCheck != PermissionChecker.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(getActivity(),
+                            new String[]{Manifest.permission.READ_SMS}, 0);
+                    // stops the action from proceeding further as permission not
+                    //  granted yet
+                    return;
+                }
+                // Create all messages URI
+                Uri uri = Uri.parse("content://sms");
+                // The columns we want
+                //  date is when the message took place
+                //  address is the number of the other party
+                //  body is the message content
+                //  type 1 is received, type 2 sent
+                String[] reqCols = new String[]{"date", "address", "body", "type"};
+
+                // Get Content Resolver object from which to
+                //  query the content provider
+                ContentResolver cr = getActivity().getContentResolver();
+                // Fetch SMS Message from Built-in Content Provider
+                // The filter String
+                String filter = "address LIKE ? ";
+                // The matches for the ?
+                String number = "%" + etNumber.getText().toString() + "%";
+                String[] filterArgs = {number};
+                // Fetch SMS Message from Built-in Content Provider
+
+                Cursor cursor = cr.query(uri, reqCols, filter, filterArgs, null);
+
+                String smsBody = "";
+
+                if (cursor.moveToFirst()) {
+                    do {
+                        long dateInMillis = cursor.getLong(0);
+                        String date = (String) DateFormat
+                                .format("dd MMM yyyy h:mm:ss aa", dateInMillis);
+                        String address = cursor.getString(1);
+                        String body = cursor.getString(2);
+                        String type = cursor.getString(3);
+                        if (type.equalsIgnoreCase("1")) {
+                            type = "Inbox:";
+                        } else {
+                            type = "Sent:";
+                        }
+                        smsBody += type + " " + address + "\n at " + date
+                                + "\n\"" + body + "\"\n\n";
+                    } while (cursor.moveToNext());
+                }
+                tvSms.setText(smsBody);
+            }
+        });
+
+        return view;
+    }
+
+}
